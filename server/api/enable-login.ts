1ec6b200b34419c3585f7a0e0f1f016d34bc4a30
yunjiyi <135610869+yunjiyi@users.noreply.github.com>
2025-03-25 23:39:47 -0700

    Update enable-login.ts

--- server/api/enable-login.ts
+++ server/api/enable-login.ts
@@ -1,8 +1,11 @@
-import process from "node:process"
+import process from "node:process";
 
 export default defineEventHandler(async () => {
+  if (process.env.ENABLE_LOGIN === "false") {
+    return { enable: false, url: null }; // 返回禁用登录的状态
+  }
   return {
     enable: true,
-    url: `https://github.com/login/oauth/authorize?client_id=${process.env.G_CLIENT_ID}`,
-  }
-})
+    url: `https://github.com/login/oauth/authorize?client_id=${process.env.G_CLIENT_ID || "default_value"}`,
+  };
+});
